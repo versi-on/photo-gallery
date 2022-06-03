12275060f76517e960b6ca69f8a165b8c254ddfb
Natalia <null>
2022-06-03 11:23:46 +0200

    add images

---
+++ src/assets/photos.js
@@ -0,0 +1,50 @@
+const photos = [
+    {
+        id: 1,
+        title: "a gull in the sky",
+        img: "gull.jpg"
+    },
+    {
+        id: 2,
+        title: "spring",
+        img: "spring.jpg"
+    },
+    {
+        id: 3,
+        title: "flower",
+        img: "green-flower.jpg"
+    },
+    {
+        id: 4,
+        title: "grapes",
+        img: "grapes.jpg"
+    },
+    {
+        id: 5,
+        title: "berlin",
+        img: "berlin.jpg"
+    },
+    {
+        id: 6,
+        title: "rain drops",
+        img: "berlin-raindrops.jpg"
+    },
+    {
+        id: 7,
+        title: "autumn",
+        img: "autumn.jpg"
+    },
+    {
+        id: 8,
+        title: "ducks",
+        img: "ducks.jpg"
+    },
+    {
+        id: 9,
+        title: "architecture",
+        img: "architecture.jpg"
+    }
+]
+
+
+export default photos
