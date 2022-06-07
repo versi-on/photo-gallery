a1f193bf1be4aeab4ac732cf0bee7eed995f283d
Natalia <null>
2022-06-07 23:06:21 +0200

    little change of desc text

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
         <header className='header'>
             <img src={logo} alt="logo" width={28} />
             <h1 className='header__title'>Photo Gallery</h1>
-            <p className='header__text'>a small photo gallery of Natalia</p>
+            <p className='header__text'>some small photo collection of Natalia</p>
         </header>
     )
 }
